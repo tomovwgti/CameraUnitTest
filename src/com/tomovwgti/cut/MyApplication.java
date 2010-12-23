866a94e65b6008eb74724f357a6b093bb1d1f377
vvakame <vvakame+dev@gmail.com>
2010-12-23 16:45:19 +0900

    例外が発生したら$EXTARNAL_DIR/CameraUnitTest 的な位置にログを吐くようにした

---
+++ src/com/tomovwgti/cut/MyApplication.java
@@ -0,0 +1,74 @@
+package com.tomovwgti.cut;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.lang.Thread.UncaughtExceptionHandler;
+
+import android.app.Application;
+import android.os.Environment;
+
+public class MyApplication extends Application {
+
+	private static File BUG_REPORT_FILE = null;
+	static {
+		File errorDir = new File(Environment.getExternalStorageDirectory(),
+				"/CameraUnitTest/ExceptionLog/");
+		if (!errorDir.exists()) {
+			if (!errorDir.mkdirs()) {
+			}
+		}
+		BUG_REPORT_FILE = new File(errorDir, String.valueOf(System
+				.currentTimeMillis()) + ".log");
+	}
+
+	/**
+	 * {@inheritDoc}<br>
+	 */
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+			UncaughtExceptionHandler defaultHandler;
+			{
+				defaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+			}
+
+			@Override
+			public void uncaughtException(Thread thread, Throwable ex) {
+				try {
+					File file = BUG_REPORT_FILE;
+					PrintWriter pw = null;
+					pw = new PrintWriter(new FileOutputStream(file));
+					saveState(ex, pw);
+					pw.flush();
+					pw.close();
+				} catch (FileNotFoundException e) {
+				}
+				defaultHandler.uncaughtException(thread, ex);
+			}
+
+			void saveState(Throwable e, PrintWriter pw)
+					throws FileNotFoundException {
+				StackTraceElement[] stacks = e.getStackTrace();
+				pw.println(e.getClass().getCanonicalName());
+				StringBuilder sb = new StringBuilder();
+				for (StackTraceElement stack : stacks) {
+					sb.setLength(0);
+					sb.append("\t");
+					sb.append(stack.getClassName()).append("#");
+					sb.append(stack.getMethodName()).append(":");
+					sb.append(stack.getLineNumber());
+					pw.println(sb.toString());
+				}
+				Throwable cause = e.getCause();
+				if (cause != null) {
+					saveState(cause, pw);
+				}
+				pw.close();
+			}
+		});
+	}
+}
